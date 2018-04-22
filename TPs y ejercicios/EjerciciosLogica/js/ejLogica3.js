2a2e75de51b968b54d299b5c2a6d767e6488f981
h242 <jimena.piano@gmail.com>
2018-04-22 20:25:39 -0300

    Ejercicios Logica3.pdf listos para corregir

--- TPs y ejercicios/EjerciciosLogica/js/ejLogica3.js
+++ TPs y ejercicios/EjerciciosLogica/js/ejLogica3.js
@@ -55,7 +55,7 @@ function ciegoZapiola() {
 estado1();
 
 
-alert("prrrrrrrrrrr");
+alert("Semaforos corriendo");
 
 // /*
 //  Modelar con pseudocÃ³digo o js:
@@ -137,116 +137,116 @@ alert("prrrrrrrrrrr");
 // 	console.log("los peatones de Zapiola pueden cruzar");
 // } 
 
-// console.log("");
-// console.log("Ej 3");
-// /*
-// Modelar con lÃ³gica y estructuras conocidas un juego de preguntas y respuestas, que conste de: 
-// - 3 rondas de 5 preguntas cada una. 
-// - Para pasar de ronda deben responderse correctamente 3 preguntas por ronda
-// */
+console.log("");
+console.log("Ej 3");
+/*
+Modelar con lÃ³gica y estructuras conocidas un juego de preguntas y respuestas, que conste de: 
+- 3 rondas de 5 preguntas cada una. 
+- Para pasar de ronda deben responderse correctamente 3 preguntas por ronda
+*/
 
-// var preguntasPorRonda, respuestasPorRonda;
-// var correctas = 0;
-// var puntaje = 0;
+var preguntasPorRonda, respuestasPorRonda;
+var correctas = 0;
+var puntaje = 0;
 
-// preguntasPorRonda = [
-// 	[
-// 		"Quién es la diosa griega del matrimonio?",
-// 		"Quién es el dios del Olimpo?", 
-// 		"Quién es el dios griego del mar?", 
-// 		"Quién es el dios griego de la guerra?", 
-// 		"Quién es la diosa griega de los cultivos?"
-// 	],
-// 	[
-// 		"Quién escribio 'El Proceso'?", 
-// 		"Quién pintó 'La Gioconda'?", 
-// 		"Quién dirijió 'E.T'?", 
-// 		"Quién compuso 'Las 4 estaciones'?", 
-// 		"Quién construyó el Jardin Botanico?"
-// 	],
-// 	[
-// 		"Qué personaje de La Odisea se presenta a una boda sin invitación?",
-// 		"Qué ciudadana de Tebas desobedece las leyes de los hombres y es condenada a muerte?",
-// 		"Quién mató a los hijos de Jason?", 
-// 		"Quién se enamora de Ulises y lo ayuda a volver a casa?", 
-// 		"quien vence a la Esfinge y es nombrado 'Salvador de Tebas'?"
-// 	]
-// ];
+preguntasPorRonda = [
+	[
+		"Quién es la diosa griega del matrimonio?",
+		"Quién es el dios del Olimpo?", 
+		"Quién es el dios griego del mar?", 
+		"Quién es el dios griego de la guerra?", 
+		"Quién es la diosa griega de los cultivos?"
+	],
+	[
+		"Quién escribio 'El Proceso'?", 
+		"Quién pintó 'La Gioconda'?", 
+		"Quién dirijió 'E.T'?", 
+		"Quién compuso 'Las 4 estaciones'?", 
+		"Quién construyó el Jardin Botanico?"
+	],
+	[
+		"Qué personaje de La Odisea se presenta a una boda sin invitación?",
+		"Qué ciudadana de Tebas desobedece las leyes de los hombres y es condenada a muerte?",
+		"Quién mató a los hijos de Jason?", 
+		"Quién se enamora de Ulises y lo ayuda a volver a casa?", 
+		"quien vence a la Esfinge y es nombrado 'Salvador de Tebas'?"
+	]
+];
 
-// respuestasPorRonda = [
-// 	[
-// 		"Hera",
-// 		"Zeus", 
-// 		"Poseidon", 
-// 		"Ares", 
-// 		"Demeter"
-// 	],
-// 	[
-// 		"Kafka", 
-// 		"Da Vinci", 
-// 		"Spielberg", 
-// 		"Vivaldi", 
-// 		"Carlos Thays"
-// 	],
-// 	[
-// 		"Eris",
-// 		"Antigona",
-// 		"Medea", 
-// 		"Circe", 
-// 		"Edipo"
-// 	]
-// ];
+respuestasPorRonda = [
+	[
+		"Hera",
+		"Zeus", 
+		"Poseidon", 
+		"Ares", 
+		"Demeter"
+	],
+	[
+		"Kafka", 
+		"Da Vinci", 
+		"Spielberg", 
+		"Vivaldi", 
+		"Carlos Thays"
+	],
+	[
+		"Eris",
+		"Antigona",
+		"Medea", 
+		"Circe", 
+		"Edipo"
+	]
+];
 
 
-// var nivel = ["Easy", "Medium", "Hard"];
-// //El valor de las respuestas correctas en la primera ronda serÃ¡ de 5 puntos, 
-// //en la segunda 10 y en la tercera 15. 
-// var puntos = [5, 10, 15];
+var nivel = ["Easy", "Medium", "Hard"];
+//El valor de las respuestas correctas en la primera ronda serÃ¡ de 5 puntos, 
+//en la segunda 10 y en la tercera 15. 
+var puntos = [5, 10, 15];
 
-// for (i = 0; i < 3; i++) {
-// 	correctas = 0;
-// 	alert("Ronda " + (i + 1) + ": " + nivel[i]);
-// 	for (j = 0; j < 5; j++) {
-// 		if (respuestasPorRonda[i][j].toLowerCase() == prompt(preguntasPorRonda[i][j]).toLowerCase()) { //convierte las rtas a todo en minuscula y lo compara con el input que viene de prompt, tambien convertido todo a minunscula.
-// 			correctas++;
-// 			puntaje += puntos[i];
-// 		}
-// 	}
-// 	//aca estamos entre rondas
-// 	if (correctas < 3) {
-// 		alert("PERDISTE");
-// 		break;
-//     } else {
-//     	alert("Tenés " + puntaje +" puntos")
-//     	continue; //podemos prescindir de esto porque igual seguirÃ­a entrando a i
-// 	}
-// }
+for (i = 0; i < 3; i++) {
+	correctas = 0;
+	alert("Ronda " + (i + 1) + ": " + nivel[i]);
+	for (j = 0; j < 5; j++) {
+		if (respuestasPorRonda[i][j].toLowerCase() == prompt(preguntasPorRonda[i][j]).toLowerCase()) { //convierte las rtas a todo en minuscula y lo compara con el input que viene de prompt, tambien convertido todo a minunscula.
+			correctas++;
+			puntaje += puntos[i];
+		}
+	}
+	//aca estamos entre rondas
+	if (correctas < 3) {
+		alert("PERDISTE");
+		break;
+    } else {
+    	alert("Tenés " + puntaje +" puntos")
+    	continue; //podemos prescindir de esto porque igual seguirÃ­a entrando a i
+	}
+}
 
-// console.log("");
-// console.log("Ej 4");
-// /*
-// Al finalizar el juego se mostrarÃ¡ un cartel al jugador de:
-// excelente si consiguiÃ³ entre 125 y 150 puntos, 
-// muy bien entre 124 y 100 puntos y 
-// bien para menos de 100 punto
-// */
-// /*
-// if (puntaje >= 125) {
-// 	alert("¡EXCELENTE! Conseguiste "+puntaje+" puntos.");
-// } else if (puntaje >= 100) {
-// 	alert("¡MUY BIEN! Conseguiste "+puntaje+" puntos.");
-// } else {
-// 	alert("BIEN. Conseguiste "+puntaje+" puntos.");
-// }
-// */
-// switch (true) { //si pongo switch(puntaje) y case (puntaje>= 125) como el if de arriba, el primer dato es int y el segundo booleano y no me anda. 
-// 	case (puntaje >= 125):
-// 		alert("¡EXCELENTE! Conseguiste "+puntaje+" puntos.");
-// 		break;
-// 	case (puntaje >= 100):
-// 		alert("MUY BIEN! Conseguiste "+puntaje+" puntos.");
-// 		break;
-// 	default:
-// 		alert("BIEN. Conseguiste "+puntaje+" puntos.");
-// 		break;
-// }
+console.log("");
+console.log("Ej 4");
+/*
+Al finalizar el juego se mostrarÃ¡ un cartel al jugador de:
+excelente si consiguiÃ³ entre 125 y 150 puntos, 
+muy bien entre 124 y 100 puntos y 
+bien para menos de 100 punto
+*/
+/*
+if (puntaje >= 125) {
+	alert("¡EXCELENTE! Conseguiste "+puntaje+" puntos.");
+} else if (puntaje >= 100) {
+	alert("¡MUY BIEN! Conseguiste "+puntaje+" puntos.");
+} else {
+	alert("BIEN. Conseguiste "+puntaje+" puntos.");
+}
+*/
+switch (true) { //si pongo switch(puntaje) y case (puntaje>= 125) como el if de arriba, el primer dato es int y el segundo booleano y no me anda. 
+	case (puntaje >= 125):
+		alert("¡EXCELENTE! Conseguiste "+puntaje+" puntos.");
+		break;
+	case (puntaje >= 100):
+		alert("MUY BIEN! Conseguiste "+puntaje+" puntos.");
+		break;
+	default:
+		alert("BIEN. Conseguiste "+puntaje+" puntos.");
+		break;
+}
